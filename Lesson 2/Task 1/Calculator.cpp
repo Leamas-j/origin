88504a3848db9e1fe1e65b294e392d781e06622d
Aleksandra <ap0433507@gmail.com>
2025-01-17 15:40:35 +0300

    adding Lesson 2

---
+++ Lesson 2/Task 1/Calculator.cpp
@@ -0,0 +1,74 @@
+﻿// Calculator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+class Calculator {
+    double num1;
+    double num2;
+public:
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else return false;       
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else return false;
+    }
+};
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    while (1) {
+        Calculator calc;
+        bool set_num;
+        do {            
+            std::cout << "Введите num1: ";
+            double num1;
+            std::cin >> num1;
+            set_num = calc.set_num1(num1);
+            if (!set_num) std::cout << "Неверный ввод!" << std::endl;
+        } while (!set_num);
+        do {            
+            std::cout << "Введите num2: ";
+            double num2;
+            std::cin >> num2;
+            set_num = calc.set_num2(num2);
+            if (!set_num) std::cout << "Неверный ввод!" << std::endl;
+        } while (!set_num);
+        std::cout << "num1 + num2 = " << calc.add() << std::endl; 
+        std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl; 
+        std::cout << "num2 * num1 = " << calc.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;   
+    }
+    return 0;
+}
---
+++ Lesson 2/Task 2/Counter.cpp
@@ -0,0 +1,73 @@
+﻿// Counter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+class Counter {
+    int num;
+public:
+    Counter(int num){
+        this->num = num;
+    }
+    Counter() {
+        num = 1;
+    }
+    void incr() {
+        ++num;
+    }
+    void decr() {
+        --num;
+    }
+    int current() {
+        return num;
+    }
+};
+
+namespace operation {
+    char increase = '+';
+    char decrease = '-';
+    char currentMeaning = '=';
+    char exit_ing = 'x';
+    char exit_rus = 'х';
+} 
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    Counter* counter;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::string answer;
+    std::cin >> answer;
+    if (answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        int initialValue;
+        std::cin >> initialValue;
+        counter = new Counter(initialValue);
+    }
+    else  counter = new Counter;
+    char command;
+    do {
+        std::cout << "Введите команду('+', '-', '=' или 'x') : ";        
+        std::cin >> command;
+        if (command == operation::increase) {
+            counter->incr();
+        }
+        else if (command == operation::decrease) {
+            counter->decr();
+        }
+        else if(command == operation::currentMeaning) {
+            std::cout << counter->current() << std::endl;
+        }
+        else if((command == operation::exit_ing) || (command == operation::exit_rus)) {
+            std::cout << "До свидания!" << std::endl;
+        }
+    } while ((command != operation::exit_ing) && (command != operation::exit_rus));
+
+    delete counter;
+    return 0;
+}
+

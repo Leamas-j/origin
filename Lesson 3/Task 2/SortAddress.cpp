141f0941ad1d810ea58919682ef58dc582e1b0a0
Aleksandra <ap0433507@gmail.com>
2025-01-22 15:10:15 +0300

    Lesson 3: change Task 1, add Task 2

--- Lesson 3/Task 2/SortAddress.cpp
+++ Lesson 3/Task 2/SortAddress.cpp
@@ -11,22 +11,27 @@ class Address {
     std::string street;
     int house;
     int flat;
-public:   
+public:
     Address(std::string city, std::string street, int house, int flat) {
         this->city = city;
         this->street = street;
         this->house = house;
         this->flat = flat;
-    }    
+    }
     std::string get_output_address() {
         std::string tmpString = city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(flat);
         return tmpString;
     }
+    std::string get_city(){
+        return city;
+    }
 };
 
+void sort(Address** addresses, int size);
+
 int main()
-{
-    SetConsoleOutputCP(1251);
+{    
+    SetConsoleOutputCP(1251);     
     Address** adr;
     int addrCount{};
     std::string lCity;
@@ -34,32 +39,45 @@ int main()
     int lHouse;
     int lFlat;
     std::ifstream fin{ "in.txt" };
-    if (fin.is_open()) {       
+    if (fin.is_open()) {
         if (fin >> addrCount) {
-            adr = new Address * [addrCount];            
+            adr = new Address * [addrCount];
             for (int i = 0; i < addrCount; ++i) {
-                fin >> lCity >> lStreet >> lHouse >> lFlat; 
-                adr[i] = new Address(lCity, lStreet, lHouse, lFlat); 
+                fin >> lCity >> lStreet >> lHouse >> lFlat;
+                adr[i] = new Address(lCity, lStreet, lHouse, lFlat);
             }
+            sort(adr, addrCount);
             std::ofstream fout{ "out.txt" };
             if (fout.is_open()) {
                 fout << addrCount << std::endl;
-                for (int i = addrCount-1; i >= 0; --i) {
+                for (int i =  0; i < addrCount; ++i) {
                     fout << adr[i]->get_output_address() << std::endl;
                 }
                 fout.close();
-               for (int i = 0; i < addrCount; ++i) {
+                for (int i = 0; i < addrCount; ++i) {
                     delete adr[i];
-               }
-               delete[] adr;
+                }
+                delete[] adr;
             }
             else {
                 std::cout << "Не получилось открыть файл! \"out.txt\"" << std::endl;
-            }           
+            }
         }
-        fin.close();        
+        fin.close();
     }
     else {
         std::cout << "Не получилось открыть файл! \"in.txt\"" << std::endl;
+    }    
+}
+
+void sort(Address** addresses, int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = i + 1; j < size; ++j) {
+            if (strcmp(addresses[i]->get_city().c_str(), addresses[j]->get_city().c_str()) > 0) {
+                Address* tmpAdr = addresses[i];
+                addresses[i] = addresses[j];
+                addresses[j] = tmpAdr;
+            }
+        }
     }
 }

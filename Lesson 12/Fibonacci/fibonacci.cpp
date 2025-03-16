279f25efbe470656004a792720260687b4727376
Aleksandra <ap0433507@gmail.com>
2025-03-16 23:39:54 +0300

    add Finabocci and FinabocciMemory

---
+++ Lesson 12/Fibonacci/fibonacci.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int fibonacci(int n)
+{
+    return n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main(){
+    SetConsoleOutputCP(1251);
+    std::cout << "Введите число: ";
+    int num{};
+    std::cin >> num;    
+
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout << fibonacci(i) << " ";
+    }
+    return 0;
+}
+
+
---
+++ Lesson 12/FibonacciMemory/fibonacciMemory.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+long long fibonacci(int n, long long* mass)
+{
+    if (mass[n] != 0) return mass[n];
+    else if (n < 2) {
+        mass[n] = n;
+    }
+    else {
+        mass[n] = fibonacci(n - 2, mass) + fibonacci(n - 1, mass);
+    }
+    return mass[n];
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите число: ";
+    int num{};
+    std::cin >> num;
+    long long* mass = new long long[num] {};
+    std::cout << std::endl;
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout << fibonacci(i, mass) << " ";
+    }
+    return 0;
+}
+
+

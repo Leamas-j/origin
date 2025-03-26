35878da2f09ae17ca9140fbbe0d0df15f2d0ca79
Aleksandra <ap0433507@gmail.com>
2025-03-26 20:43:48 +0300

    change mergeSort

--- Lesson 13/mergeSort/mergeSort.cpp
+++ Lesson 13/mergeSort/mergeSort.cpp
@@ -1,79 +1,88 @@
 ﻿#include <iostream>
+#include <vector>
+
 #include "Windows.h"
 
-int* merge(int* arr1, int size1, int* arr2, int size2);
-int* merge_sort(int* arr, int size);
-void printArr(int* arr, int size);
+std::vector<int> merge(std::vector<int> arr1, int size1, std::vector<int> arr2, int size2);
+std::vector<int> merge_sort(std::vector<int> arr, int size);
+void printArr(std::vector<int>arr);
 
 int main()
 {
-    SetConsoleOutputCP(1251); 
+    SetConsoleOutputCP(1251);
+    /*
+        const int size{10};
+        int arr[size]{ 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };
+    */
+    /*
+       const int size{15};
+       int arr[size]{ 88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25 ,74 };
+    */
+    const int size{ 18 };
+    int arr[size]{ 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
 
-    int size[3]{10,15,18};
-
-    int** arrAll = new int*[3];
-    arrAll[0] = new int[size[0]]{ 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };    
-    arrAll[1] = new int[size[1]]{ 88, 91, 87, 59, 53, 49, 29, 16, 4, 27, 28, 89, 2, 25 ,74 };   
-    arrAll[2] = new int[size[2]] { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
-
-    int* arB;
-    for (int i = 0; i < 3; ++i) {
-        std::cout << "Исходный массив: ";
-        printArr(arrAll[i], size[i]);
-        arB = merge_sort(arrAll[i], size[i]);
-        std::cout << "Отсортированный массив: ";
-        printArr(arB, size[i]);
-        delete[] arB; 
-        std::cout << std::endl;
+    std::vector<int> arrA;
+    for (int i = 0; i < size; ++i) {
+        arrA.push_back(arr[i]);
     }
-    for (int i = 0; i < 3; ++i) {
-        delete[] arrAll[i];
-    }
-    delete[] arrAll;
+    std::vector<int> arB;
+
+    std::cout << "Исходный массив: ";
+    printArr(arrA);
+    arB = merge_sort(arrA, size);
+    std::cout << "Отсортированный массив: ";
+    printArr(arB);
+    std::cout << std::endl;
     return 0;
 }
 
-int* merge_sort(int* arr, int size) {
+std::vector<int> merge_sort(std::vector<int> arr, int size) {
     if (size == 1) return arr;
     else {
         int s1 = static_cast<int>(size / 2);
         int s2 = size - s1;
-        int* left, * right;
-        left = merge_sort(arr, s1);
-        right = merge_sort(&arr[s1], s2);
+        std::vector<int> left, right;
+        for (int i = 0; i < size; ++i) {
+            if (i < s1) {
+                left.push_back(arr.at(i));
+            }
+            else {
+                right.push_back(arr.at(i));
+            }
+        }
+        left = merge_sort(left, s1);
+        right = merge_sort(right, s2);
         return merge(left, s1, right, s2);
     }
 }
 
-void printArr(int* arr, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
+void printArr(std::vector<int> arr) {
+    for (const auto v : arr) {
+        std::cout << v << " ";
     }
     std::cout << std::endl;
 }
 
-int* merge(int* arrA, int sizeA, int* arrB, int sizeB) {
-    int ia = 0, ib = 0, ic = 0;
-    int sizeRes = sizeA + sizeB;
-    int* arrRes = new int[sizeRes];
+std::vector<int> merge(std::vector<int> arrA, int sizeA, std::vector<int> arrB, int sizeB) {
+    int ia = 0, ib = 0;
+    std::vector<int> arrRes;
     while ((ia < sizeA) || (ib < sizeB)) {
         if (ia == sizeA) {
-            arrRes[ic] = arrB[ib];
+            arrRes.push_back(arrB.at(ib));
             ib++;
         }
         else if (ib == sizeB) {
-            arrRes[ic] = arrA[ia];
+            arrRes.push_back(arrA.at(ia));
             ia++;
         }
-        else if (arrA[ia] <= arrB[ib]) {
-            arrRes[ic] = arrA[ia];
+        else if (arrA.at(ia) <= arrB.at(ib)) {
+            arrRes.push_back(arrA.at(ia));
             ia++;
         }
         else {
-            arrRes[ic] = arrB[ib];           
+            arrRes.push_back(arrB.at(ib));
             ib++;
         }
-        ic++;
     }
     return arrRes;
 }

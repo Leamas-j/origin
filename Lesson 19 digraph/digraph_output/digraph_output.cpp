6e9477a4e3189dde810f67555f5a5abcb4019af7
Aleksandra <ap0433507@gmail.com>
2025-04-08 22:19:29 +0300

    add lesson 19 digraph

---
+++ Lesson 19 digraph/digraph_output/digraph_output.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Windows.h"
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    std::ifstream fin{ "input.txt" };
+    int vertexCount;
+    if (fin.is_open()) {
+        fin >> vertexCount;
+        int** digraph = new int* [vertexCount];
+        for (int i = 0; i < vertexCount; ++i) {
+            digraph[i] = new int[vertexCount];
+        }
+        for (int i = 0; i < vertexCount; ++i) {
+            for (int j = 0; j < vertexCount; ++j) {
+                fin >> digraph[i][j];
+            }
+        }
+        fin.close(); 
+        std::cout << "Текстовый вид орграфа:\n";
+        for (int i = 0; i < vertexCount; ++i) {
+            bool findArc{ false };
+            std::cout << i + 1 << ": ";
+            for (int j = 0; j < vertexCount; ++j) {
+                if (digraph[i][j] == 1) {
+                    findArc = true;
+                    std::cout << j + 1 << " ";
+                }                    
+            }
+            if (!findArc) {
+                std::cout << "нет";
+            } 
+            std::cout << std::endl;
+        }
+       
+        std::cout << std::endl;
+        for (int i = 0; i < vertexCount; ++i) {
+            delete[] digraph[i];
+        }
+        delete[] digraph;
+    }
+    else {
+        std::cout << "Не удалось открыть файл input.txt" << std::endl;
+    }
+}
---
+++ Lesson 19 digraph/digraph_topological_sorting/digraph_topological_sorting.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include "Windows.h"
+
+void depth_first_search(int** graph, const int vertex, int* visited, const int vertexCount, std::list<int>& sortVert);
+int main()
+{
+    SetConsoleOutputCP(1251);
+    std::ifstream fin{ "input.txt" };
+    int vertexCount;
+    if (fin.is_open()) {
+        fin >> vertexCount;
+        int** graph = new int* [vertexCount];
+        for (int i = 0; i < vertexCount; ++i) {
+            graph[i] = new int[vertexCount];
+        }
+        for (int i = 0; i < vertexCount; ++i) {
+            for (int j = 0; j < vertexCount; ++j) {
+                fin >> graph[i][j];
+            }
+        }
+        fin.close();
+        int* visitedVertex = new int[vertexCount] {0};
+        std::list<int> sortedVertex;
+        for (int i = 0; i < vertexCount; ++i) {
+            if (!visitedVertex[i]) {
+                depth_first_search(graph, i, visitedVertex, vertexCount, sortedVertex);
+            }
+        }
+        std::cout << "Топологический порядок вершин: ";
+        for (int k : sortedVertex) {
+            std::cout << k << " ";
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < vertexCount; ++i) {
+            delete[] graph[i];
+        }
+        delete[] graph;
+        delete[] visitedVertex;
+    }
+    else {
+        std::cout << "Не удалось открыть файл input.txt" << std::endl;
+    }
+}
+
+void depth_first_search(int** graph, const int vertex, int* visited, const int vertexCount, std::list<int>& sortVert) {
+    visited[vertex] = 1;
+    for (int i = 0; i < vertexCount; ++i) {
+        if (graph[vertex][i] == 1) {
+            if (!visited[i]) {
+                depth_first_search(graph, i, visited, vertexCount, sortVert);
+            }            
+        }        
+    }
+    sortVert.push_front(vertex + 1);
+}

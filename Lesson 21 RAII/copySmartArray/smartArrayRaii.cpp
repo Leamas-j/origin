d9ad5120355328e02ec56f8861ca1e55c5521eaf
Aleksandra <ap0433507@gmail.com>
2025-04-28 22:49:55 +0300

    change Lesson 21

--- Lesson 21 RAII/copySmartArray/smartArrayRaii.cpp
+++ Lesson 21 RAII/copySmartArray/smartArrayRaii.cpp
@@ -4,62 +4,65 @@
 
 class smart_array {
 public:
-    smart_array(int size) {
-		this->current = 0;
-		this->size = size;
-		data = new int[size] {0};
-    }
-	void add_element(int elem) {
-		if (current < size) {
-			data[current] = elem;
-			current++;
-		}
-		else throw std::exception("Выход за рамки массива!");
+  smart_array(int size) {
+	if (size > 0) {
+	  this->current = 0;
+	  this->size = size;
+	  data = new int[size] {0};
 	}
-	int get_element(int elemNum) {
-		if (elemNum < size) {
-			return data[elemNum];
-		}
-		else throw std::exception("Выход за рамки массива!");
+	else throw std::exception("Недопустимый размер массива!");
+  }
+void add_element(int elem) {
+  if (current < size) {
+    data[current] = elem;
+	current++;
+  }
+  else throw std::exception("Выход за рамки массива!");
+}
+int get_element(int elemNum) {
+  if ((elemNum < size) && (elemNum >= 0)) {
+    return data[elemNum];
+  }
+  else throw std::exception("Выход за рамки массива!");
+}
+smart_array& operator =(const smart_array& other) {
+  if (this != &other) {
+	delete[] this->data;
+	this->size = other.size;
+	this->current = other.current;
+	this->data = new int[size];
+	for (int i = 0; i < size; ++i) {
+	  this->data[i] = other.data[i];
 	}
-	smart_array& operator =(const smart_array& other) {
-		if (this != &other) {
-			delete[] this->data;
-			this->size = other.size;
-			this->current = other.current;
-			this->data = new int[size];
-			for (int i = 0; i < size; ++i) {
-				this->data[i] = other.data[i];
-			}
-		}
-		return *this;
-	}
-    ~smart_array() {
-        delete[] data;
-    }
+  }
+  return *this;
+}
+~smart_array() {
+  delete[] data;
+}
 private:
-	int current;
-	int size;
-    int* data{ nullptr };
- };
+  int current;
+  int size;
+  int* data{ nullptr };
+};
 
 int main()
 {
-	SetConsoleOutputCP(1251);
-	try {
-		smart_array arr(5);
-		arr.add_element(1);
-		arr.add_element(4);
-		arr.add_element(155);
+  SetConsoleOutputCP(1251);
+  try {
+	smart_array arr(5);
+	arr.add_element(1);
+	arr.add_element(4);
+	arr.add_element(155);
 
-		smart_array new_array(2);
-		new_array.add_element(44);
-		new_array.add_element(34);
-
-		arr = new_array;		
-	}
-	catch (const std::exception& ex) {
-		std::cout << ex.what() << std::endl;
-	}
+	smart_array new_array(2);
+	new_array.add_element(44);
+	new_array.add_element(34);
+	
+	arr = new_array;		
+  }
+  catch (const std::exception& ex) {
+	std::cout << ex.what() << std::endl;
+  }
 }
 
--- Lesson 21 RAII/smartArrayRaii/smartArrayRaii.cpp
+++ Lesson 21 RAII/smartArrayRaii/smartArrayRaii.cpp
@@ -4,47 +4,62 @@
 
 class smart_array {
 public:
-    smart_array(int size) {
-		this->current = 0;
-		this->size = size;
-		data = new int[size] {0};
-    }
-	void add_element(int elem) {
-		if (current < size) {
-			data[current] = elem;
-			current++;
-		}
-		else throw std::exception("Выход за рамки массива!");
+  smart_array(int size) {
+	if (size > 0) {
+	  this->current = 0;
+	  this->size = size;
+	  data = new int[size] {0};
 	}
-	int get_element(int elemNum) {
-		if (elemNum < size) {
-			return data[elemNum];
-		}
-		else throw std::exception("Выход за рамки массива!");
+	else throw std::exception("Недопустимый размер массива!");
+  }
+void add_element(int elem) {
+  if (current < size) {
+    data[current] = elem;
+	current++;
+  }
+  else throw std::exception("Выход за рамки массива!");
+}
+int get_element(int elemNum) {
+  if ((elemNum < size) && (elemNum >= 0)) {
+    return data[elemNum];
+  }
+  else throw std::exception("Выход за рамки массива!");
+}
+smart_array& operator =(const smart_array& other) {
+  if (this != &other) {
+	delete[] this->data;
+	this->size = other.size;
+	this->current = other.current;
+	this->data = new int[size];
+	for (int i = 0; i < size; ++i) {
+	  this->data[i] = other.data[i];
 	}
-    ~smart_array() {
-        delete[] data;
-    }
+  }
+  return *this;
+}
+~smart_array() {
+  delete[] data;
+}
 private:
-	int current;
-	int size;
-    int* data{ nullptr };
- };
+  int current;
+  int size;
+  int* data{ nullptr };
+};
 
 int main()
 {
-	SetConsoleOutputCP(1251);
-	try {
-		smart_array arr(5);
-		arr.add_element(1);
-		arr.add_element(4);
-		arr.add_element(155);
-		arr.add_element(14);
-		arr.add_element(15);
-		std::cout << arr.get_element(1) << std::endl;
-	}
-	catch (const std::exception& ex) {
-		std::cout << ex.what() << std::endl;
-	}
+  SetConsoleOutputCP(1251);
+  try {
+	smart_array arr(5);
+	arr.add_element(1);
+	arr.add_element(4);
+	arr.add_element(155);
+	arr.add_element(14);
+	arr.add_element(15);
+	std::cout << arr.get_element(1) << std::endl;
+  }
+  catch (const std::exception& ex) {
+	std::cout << ex.what() << std::endl;
+  }
 }
 

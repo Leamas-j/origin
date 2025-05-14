bf7ff720cf79272901e40adbe518e8b9d48bfae6
Aleksandra <ap0433507@gmail.com>
2025-05-14 22:59:17 +0300

    change count_simb

--- Lesson 24 STL 1/count_simb/count_simb.cpp
+++ Lesson 24 STL 1/count_simb/count_simb.cpp
@@ -1,18 +1,22 @@
 ﻿#include <iostream>
 #include <set>
 #include <map>
+#include <unordered_set>
 
 int main()
 {
-  std::multiset<char> mess{ 'H','e','l','l','o',' ','w','o','r','l','d','!','!' };
-  std::set<char> unMess{ 'H','e','l','l','o',' ','w','o','r','l','d','!','!' };
-
+  std::string str{ "Hello world!!" };
+  std::multiset<char> mess{ str.begin(), str.end()};
+  std::unordered_set<char> unMess{ mess.begin(), mess.end()};
   std::multimap<int, char, std::greater<int>> res;
+
   for (const auto& k : unMess) {
     res.insert(std::make_pair(mess.count(k), k));
   }
+
   for (const auto& k : res) {
     std::cout << k.second << ": " << k.first << std::endl;
   }
+
   return 0;
 }

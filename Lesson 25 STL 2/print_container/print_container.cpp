d13e55bb8910ccff5ec3eec53bf72eea296f7c63
Aleksandra <ap0433507@gmail.com>
2025-05-18 23:15:02 +0300

    add Lesson 25 STL 2

---
+++ Lesson 25 STL 2/print_container/print_container.cpp
@@ -0,0 +1,28 @@
+﻿#include <set>
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+
+template <typename T> 
+void print_container(const T& s) {
+
+  for (const auto& k : s) {
+    std::cout <<  k << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main()
+{
+  std::set<std::string> test_set = { "one", "two", "three", "four" };
+  print_container(test_set);
+
+  std::list<std::string> test_list = { "one", "two", "three", "four" };
+  print_container(test_list);
+
+  std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+  print_container(test_vector);
+
+  return 0;
+}
---
+++ Lesson 25 STL 2/unique_vector/unique_vector.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+  std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+  std::sort(begin(vec), end(vec));
+
+  vec.erase(std::unique(begin(vec), end(vec)), end(vec));
+
+  for (int k : vec) {
+    std::cout << k << " ";
+  }
+
+  return 0;
+}
+

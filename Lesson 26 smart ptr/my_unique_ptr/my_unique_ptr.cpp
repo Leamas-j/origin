eee729d96ea09a852e976176d5fd15b269675ee4
Aleksandra <ap0433507@gmail.com>
2025-05-23 22:22:00 +0300

    add Lesson 26 smart ptr

---
+++ Lesson 26 smart ptr/my_unique_ptr/my_unique_ptr.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+template <typename T>
+class myUniquePtr {
+public:
+ 
+  myUniquePtr(T* ptr) : ptr_(ptr) {}
+  
+  T& operator*() const {
+    return *ptr_;
+  }
+
+  myUniquePtr& operator=(const myUniquePtr&) = delete;
+ 
+  myUniquePtr(const myUniquePtr&) = delete;
+
+  T* release() {
+    T* ptr = ptr_;
+    ptr_ = nullptr;
+    return ptr;
+  } 
+
+  ~myUniquePtr() {
+    delete ptr_;
+  }
+
+private:
+  T* ptr_;
+};
+
+int main() {
+  myUniquePtr<int> p(new int(8));
+
+  std::cout << *p << std::endl;  
+
+  int* s_ptr = p.release(); 
+
+  std::cout << *s_ptr << std::endl; 
+
+  delete s_ptr; 
+
+  return 0;
+}

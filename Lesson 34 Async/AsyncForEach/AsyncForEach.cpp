b76892800a16d3a7729d17d7203d92026e7f5254
Aleksandra <ap0433507@gmail.com>
2025-11-09 14:04:47 +0300

    add Lesson 34

---
+++ Lesson 34 Async/AsyncForEach/AsyncForEach.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <future>
+
+template<typename Iterator, typename Func>
+void parallel_for_each(Iterator first, Iterator last, Func &func)
+{
+  auto size = std::distance(first, last);
+  if (size < 10)
+  {   
+    std::for_each(first, last, func);
+    return;
+  }
+  else
+  {
+    Iterator middle = first;
+    std::advance(middle, size / 2);
+
+    auto first_half = async(parallel_for_each<Iterator, Func>, first, middle, std::ref(func));
+
+    parallel_for_each(middle, last, func);
+  }
+}
+
+void print_vector(int n)
+{
+  std::cout << n << " ";
+}
+
+int main()
+{
+  std::random_device rd;
+  std::mt19937 gen{ rd() };
+  std::uniform_int_distribution<> dis(0, 100);
+
+  std::vector<int> vec;
+
+  for (int i = 0; i < 35; ++i) {
+    vec.push_back(dis(gen));
+  }
+
+  parallel_for_each(vec.begin(), vec.end(), print_vector);
+
+  std::cout << std::endl;
+
+  return 0;
+}
---
+++ Lesson 34 Async/AsyncSort/AsyncSort.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+#include <thread>
+#include <future>
+
+void printVector(const std::vector<int> &v)
+{
+	for (const auto& t : v) {
+		std::cout << t << " ";
+	}
+	std::cout << std::endl;	
+}
+
+void findMin(const std::vector<int> &v, const int i, int &pos, std::promise<int> pr)
+{
+	int min = v[i];
+	for (int j = i; j < v.size(); ++j)
+	{	
+		if (v[j] <= min)
+		{
+			min = v[j];
+			pos = j;
+		}
+	}
+	pr.set_value(min);
+}
+
+int main()
+{
+	std::random_device rd;
+	std::mt19937 gen{ rd() };
+	std::uniform_int_distribution<> dis(0, 100);
+	
+	std::vector<int> vec;
+
+	for (int i = 0; i < 15; ++i) {
+		vec.push_back( dis(gen));
+	}
+
+	printVector(vec);
+
+	for (int i = 0; i < vec.size(); ++i)
+	{		
+		int pos = 0;
+
+		std::promise<int> prom;
+		std::future<int> min = prom.get_future();
+		
+		auto res = async(findMin, std::ref(vec), i, std::ref(pos), move(prom));
+		min.wait();
+
+		vec[pos] = vec[i];
+		vec[i] = min.get();		
+	}
+
+	printVector(vec);	
+}
